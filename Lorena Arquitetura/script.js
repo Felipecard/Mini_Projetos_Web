55079bb5ed55b6e1ea2e6137d010211d3df22267
Felipe Cardoso <57718840+Felipecard@users.noreply.github.com>
2020-09-15 12:18:36 -0300

    Envio do projeto

---
+++ Lorena Arquitetura/script.js
@@ -0,0 +1,8 @@
+
+$('#collapse-navbar').on('show.bs.collapse', function(){ 
+    $('.lorena-banner').css('transform', 'translate(-50%, -10%')
+})
+
+$('#collapse-navbar').on('hide.bs.collapse', function(){ 
+    $('.lorena-banner').css('transform', 'translate(-50%, -50%')
+})
